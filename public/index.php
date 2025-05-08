6f355ab784338d9caf16614d580d04891be31338
SOS <>
2025-05-08 16:55:34 +0200

    New endpoints added

--- public/index.php
+++ public/index.php
@@ -170,8 +170,12 @@ $app->post('/fetchTimeRecords', function (Request $request, Response $response, 
                     'common_comment_internal' => $posts['fieldData']['common_comment_internal'] ?? null,
                     'time_employee_id' => $posts['fieldData']['time_employee_id'] ?? null,
                     'recordId' => $posts['recordId'] ?? null,
-                    'time_chargeable' => $post['fieldData']['time_chargeable'] ?? null,
-                    'time_not_worked' => $post['fieldData']['time_not_worked'] ?? null
+                    'time_chargeable' => $posts['fieldData']['time_chargeable'] ?? null,
+                    'time_not_worked' => $posts['fieldData']['time_not_worked'] ?? null,
+                    'common_item_price' => $posts['fieldData']['common_item_price'] ?? null,
+                    'time_eventuser::eventuser_done' => $posts['fieldData']['time_eventuser::eventuser_done'] ?? null,
+                    '!ID' => $posts['fieldData']['!ID'] ?? null,
+                    '!todo' => $posts['fieldData']['!todo'] ?? null
 
                 ];
             }, $responseBody['response']['data']);
@@ -184,6 +188,231 @@ $app->post('/fetchTimeRecords', function (Request $request, Response $response, 
     } else {
         $response->getBody()->write(json_encode([
             "error" => "Failed to fetch posts",
+            "status_code" => $httpCode
+        ]));
+    }
+
+    return $response->withHeader('Content-Type', 'application/json')->withStatus($httpCode);
+});
+
+// FETCH TODO
+
+$app->post('/fetchTodo', function (Request $request, Response $response, $args) {
+
+    $authHeader = $request->getHeaderLine('Authorization');
+    // $data = $request->getParsedBody();
+    $token = substr($authHeader, 7);
+
+    if (empty($authHeader)) {
+        return $response->withStatus(400, 'Authorization header is missing')->withHeader('Content-Type', 'application/json');
+    }
+
+    if (!$token) {
+        $response->getBody()->write(json_encode(["Error" => "Missing Token"]));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
+    }
+
+    $headers = [
+        "Authorization: bearer $token",
+        'Content-Type: application/json'
+    ];
+
+    $data = json_decode($request->getBody()->getContents(), true);
+
+    if (!isset($data['query']) || !is_array($data['query'])) {
+        return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+    };
+
+    $url = "https://hp5.positionett.se/fmi/data/vLatest/databases/PositionEtt_P1PR_PROJECT/layouts/todoDataAPI/_find";
+
+    $ch = curl_init($url);
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+    $filemakerResponse = curl_exec($ch);
+    $httpCode = curl_getInfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if ($httpCode === 200) {
+        $responseBody = json_decode($filemakerResponse, true);
+
+        if (isset($responseBody['response']['data'])) {
+
+            // Apply filter and remove any todo_done that is "1"
+            $filteredResponse = array_filter($responseBody['response']['data'], function ($posts) {
+                // Ensure that todo_done is not "1"
+                return $posts['fieldData']['todo_done'] !== "1";
+            });
+
+            $filteredResponse = array_map(function ($posts) {
+                return [
+                    '!ID' => $posts['fieldData']['!ID'] ?? null,
+                    'todo_date' => $posts['fieldData']['todo_date'] ?? null,
+                    'todo_head' => $posts['fieldData']['todo_head'] ?? null,
+                    'todo_text' => $posts['fieldData']['todo_text'] ?? null,
+                    'todo_start' => $posts['fieldData']['todo_start'] ?? null,
+                    'todo_stop' => $posts['fieldData']['todo_stop'] ?? null,
+                    'todo_done' => $posts['fieldData']['todo_done'] ?? null,
+                    'recordId' => $posts['recordId'] ?? null,
+                    'todo_arendenr' => $posts['fieldData']['todo_arendenr'] ?? null,
+                    '!common_our_reference' => $posts['fieldData']['!common_our_reference'] ?? null,
+                    // 'zb_jsonEvent' => $posts['fieldData']['zb_jsonEvent'] ?? null
+                ];
+            }, $filteredResponse);
+
+            $response->getBody()->write(json_encode(["data" => $filteredResponse]));
+        } else {
+
+            $response->getBody()->write(json_encode(["error" => "No todo data found"]));
+        }
+    } else {
+        $response->getBody()->write(json_encode([
+            "error" => "Failed to fetch todo posts",
+            "status_code" => $httpCode
+        ]));
+    }
+
+    return $response->withHeader('Content-Type', 'application/json')->withStatus($httpCode);
+});
+
+$app->post('/fetchEvents', function (Request $request, Response $response, $args) {
+
+    $authHeader = $request->getHeaderLine('Authorization');
+    // $data = $request->getParsedBody();
+    $token = substr($authHeader, 7);
+
+    if (empty($authHeader)) {
+        return $response->withStatus(400, 'Authorization header is missing')->withHeader('Content-Type', 'application/json');
+    }
+
+    if (!$token) {
+        $response->getBody()->write(json_encode(["Error" => "Missing Token"]));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
+    }
+
+    $headers = [
+        "Authorization: bearer $token",
+        'Content-Type: application/json'
+    ];
+
+    $data = json_decode($request->getBody()->getContents(), true);
+
+    if (!isset($data['query']) || !is_array($data['query'])) {
+        return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+    };
+
+    $url = "https://hp5.positionett.se/fmi/data/vLatest/databases/PositionEtt_P1PR_PROJECT/layouts/EventAPI/_find";
+
+    $ch = curl_init($url);
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+    $filemakerResponse = curl_exec($ch);
+    $httpCode = curl_getInfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if ($httpCode === 200) {
+        $responseBody = json_decode($filemakerResponse, true);
+
+        if (isset($responseBody['response']['data'])) {
+
+            $filteredResponse = array_map(function ($posts) {
+                return [
+                    '!ID' => $posts['fieldData']['!ID'] ?? null,
+                    '!todo' => $posts['fieldData']['!todo'] ?? null,
+                    'event_date_end' => $posts['fieldData']['event_date_end'] ?? null,
+                    'event_date_start' => $posts['fieldData']['event_date_start'] ?? null,
+                    'event_time_end' => $posts['fieldData']['event_time_end'] ?? null,
+                    'event_time_start' => $posts['fieldData']['event_time_start'] ?? null    
+                ];
+            }, $responseBody['response']['data']);
+
+            $response->getBody()->write(json_encode(["data" => $filteredResponse]));
+        } else {
+
+            $response->getBody()->write(json_encode(["error" => "No todo data found"]));
+        }
+    } else {
+        $response->getBody()->write(json_encode([
+            "error" => "Failed to fetch event posts",
+            "status_code" => $httpCode
+        ]));
+    }
+
+    return $response->withHeader('Content-Type', 'application/json')->withStatus($httpCode);
+});
+
+$app->post('/fetchEventUsers', function (Request $request, Response $response, $args) {
+
+    $authHeader = $request->getHeaderLine('Authorization');
+    // $data = $request->getParsedBody();
+    $token = substr($authHeader, 7);
+
+    if (empty($authHeader)) {
+        return $response->withStatus(400, 'Authorization header is missing')->withHeader('Content-Type', 'application/json');
+    }
+
+    if (!$token) {
+        $response->getBody()->write(json_encode(["Error" => "Missing Token"]));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
+    }
+
+    $headers = [
+        "Authorization: bearer $token",
+        'Content-Type: application/json'
+    ];
+
+    $data = json_decode($request->getBody()->getContents(), true);
+
+    if (!isset($data['query']) || !is_array($data['query'])) {
+        return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+    };
+
+    $url = "https://hp5.positionett.se/fmi/data/vLatest/databases/PositionEtt_P1PR_PROJECT/layouts/EventuserAPI/_find";
+
+    $ch = curl_init($url);
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+    $filemakerResponse = curl_exec($ch);
+    $httpCode = curl_getInfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if ($httpCode === 200) {
+        $responseBody = json_decode($filemakerResponse, true);
+
+        if (isset($responseBody['response']['data'])) {
+
+            $filteredResponse = array_map(function ($posts) {
+                return [
+                    '!user' => $posts['fieldData']['!user'] ?? null,
+                    '!ID' => $posts['fieldData']['!ID'] ?? null,
+                    '!event' => $posts['fieldData']['!event'] ?? null,
+                    'eventuser_done' => $posts['fieldData']['eventuser_done'] ?? null,
+                    
+                ];
+            }, $responseBody['response']['data']);
+
+            $response->getBody()->write(json_encode(["data" => $filteredResponse]));
+        } else {
+
+            $response->getBody()->write(json_encode(["error" => "No todo data found"]));
+        }
+    } else {
+        $response->getBody()->write(json_encode([
+            "error" => "Failed to fetch event user posts",
             "status_code" => $httpCode
         ]));
     }
